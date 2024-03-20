fb68d452dd8c47b007eeddd0a6b95e8df65ecb66
Beeporb <backplateorbit@gmail.com>
2024-03-20 16:16:07 +0000

    feat: add log dir var to config

--- gingko/gingko/config.py
+++ gingko/gingko/config.py
@@ -5,6 +5,7 @@ import os
 import pathlib
 
 GINGKO_INPUT_DIR = pathlib.Path(os.getenv("GINGKO_INPUT_DIR", "/mnt/data"))
+GINGKO_LOGGING_DIR = pathlib.Path(os.getenv("GINGKO_LOGGING_DIR"), "/var/log/gingko")
 
 GINGKO_REDIS_HOST = os.getenv("GINGKO_REDIS_HOST", "redis")
 GINGKO_REDIS_PORT = os.getenv("GINGKO_REDIS_PORT", "6379")
